c74265981730628692f911f4766a52e29ee919b8
AngelGamez2021 <agamez6@unisimon.edu.co>
2022-06-02 17:31:33 -0500

    primer avance con pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  titles: any[] = ['Variable', 'Pipes','Salida'];
+  
+  nombre: string = 'Capitan America';
+
+  arrayNumber = [1,2,3,4,5,6,7,8,9,10];
+
+  PI: number = Math.PI;
+
+  porcentaje: number = 0.234;
+
+  salario: number = 1234.5;
+
+  
+  valorPromesa = new Promise<string>((resolve)=>{
+  
+    setTimeout(()=>{
+        resolve('LLegó la data')
+    },4500)
+
+  })
+
+  fecha: Date = new Date();
+
+
+
+
+
+
+
+  heroe = {
+    nombre: 'Gokú',
+    clave: 'Sayayin',
+    edad: 41,
+    direccion: {
+      calle: 'Montaña Pao',
+      casa: 1
+    }
+  }
+
+
+
+
+ 
 }
